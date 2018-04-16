fbf49aa26d7fec180a45a7079ba9cbb00ad7058c
Jess Brown <jesbro@umich.edu>
2018-04-16 14:02:26 -0400

    js update

---
+++ final/js/interactivity.js
@@ -0,0 +1,9 @@
+function imageHide(x) {
+    x.classList.toggle("change");
+    document.getElementById("pic1").style.display = "none";
+}
+
+function imageShow(x) {
+    x.classList.toggle("change");
+    document.getElementById("pic1").style.display = "block";
+}
